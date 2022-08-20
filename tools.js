99491e07f21191189ae4c3d13206d5c7e64a4995
tushar motka <70910839+tushar2481@users.noreply.github.com>
2022-08-20 20:24:34 +0530

    Add files via upload

---
+++ tools.js
@@ -0,0 +1,7 @@
+function open_dropdown() {
+    document.getElementById('dropdown-content').style.display = "block";
+}
+
+function close_dropdown() {
+    document.getElementById('dropdown-content').style.display = "none";
+}
