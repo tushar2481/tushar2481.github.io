f543b7dec163bfadbddc9131e609ff5293b7acb9
tushar motka <70910839+tushar2481@users.noreply.github.com>
2022-10-17 03:54:37 +0530

    Create tool.js

---
+++ scripts/tool.js
@@ -0,0 +1,17 @@
+function nmap() {
+    document.getElementById('ip1').style.display = 'block';
+    document.getElementById('ip2').style.display = 'block';
+    document.getElementById('ip3').style.display = 'block';
+}
+
+function assetfinder() {
+    document.getElementById('ip1').style.display = 'block';
+    document.getElementById('ip2').style.display = 'none';
+    document.getElementById('ip3').style.display = 'none';
+}
+
+function dnsenum() {
+    document.getElementById('ip1').style.display = 'block';
+    document.getElementById('ip2').style.display = 'block';
+    document.getElementById('ip3').style.display = 'block';
+}
