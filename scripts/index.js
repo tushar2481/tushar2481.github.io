1e0426af8e14be5757a5298415936d51692f7b26
tushar motka <70910839+tushar2481@users.noreply.github.com>
2022-10-17 03:54:16 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,18 @@
-function nmap() {
-    nmap1();
-    document.getElementById('ip1').style.display = 'block';
-    document.getElementById('ip2').style.display = 'block';
-    document.getElementById('ip3').style.display = 'block';
-    document.getElementById('sub-btn').style.display = 'block'; 
-}
-
-function assetfinder() {
-    assetf1();
-    document.getElementById('ip1').style.display = 'block';
-    document.getElementById('ip2').style.display = 'none';
-    document.getElementById('ip3').style.display = 'none';
-    document.getElementById('sub-btn').style.display = 'block';
-}
-
-function dnsenum() {
-    dnse1();
-    document.getElementById('ip1').style.display = 'block';
-    document.getElementById('ip2').style.display = 'block';
-    document.getElementById('ip3').style.display = 'block';
-    document.getElementById('sub-btn').style.display = 'block';
-}
-
-function nmap1(){   
+function nmap1() {
     document.getElementById('tool_name').setAttribute('value', 'nmap');
     document.getElementById('ip1').placeholder = "Enter IP Address";
     document.getElementById('ip2').placeholder = "Number of threads (1-5)";
     document.getElementById('ip3').placeholder = "Verbose Mode (yes/no)";
 }
 
-function assetf1(){
-    document.getElementById('ip1').placeholder = "Enter Domain Name";
+function assetf1() {
     document.getElementById('tool_name').setAttribute('value', 'assetfinder');
+    document.getElementById('ip1').placeholder = "Enter Domain Name";
 }
 
-function dnse1(){
-    document.getElementById('tool_name').setAttribute('value', 'dnsenum');
+function dnse1() {
+    document.getElementById('tool_name').setAttribute('value', 'nmap');
     document.getElementById('ip1').placeholder = "Enter Domain Name";
-    document.getElementById('ip2').placeholder = "Enable Zone Transfer? (Y/n)";
-    document.getElementById('ip3').placeholder = "Enable Verbose Mode? (Y/n)";
+    document.getElementById('ip2').placeholder = "Zone Transfer? (yes/no)";
+    document.getElementById('ip3').placeholder = "Verbose Mode (yes/no)";
 }
