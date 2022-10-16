e1de4de76697e1a121f4c9d3da19fc4e804d1316
tushar motka <70910839+tushar2481@users.noreply.github.com>
2022-10-17 00:01:17 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 function nmap() {
-    document.getElementById('ip1').style.display = 'block';
-    document.getElementById('ip2').style.display = 'block';
-    document.getElementById('ip3').style.display = 'block';
-    document.getElementById('sub-btn').style.display = 'block';
-    document.getElementById('tool_name').setAttribute('value', 'nmap'); 
+    document.getElementById('tool_name').setAttribute('value', 'nmap');
     document.getElementById('ip1').placeholder = "Enter IP Address";
     document.getElementById('ip2').placeholder = "Number of threads (1-5)";
     document.getElementById('ip3').placeholder = "Verbose Mode (yes/no)";
+    document.getElementById('ip1').style.display = 'block';
+    document.getElementById('ip2').style.display = 'block';
+    document.getElementById('ip3').style.display = 'block';
+    document.getElementById('sub-btn').style.display = 'block'; 
 }
 
 function assetfinder() {
