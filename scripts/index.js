45aea82056415d6572a99db2aee6d80c8db10a0e
tushar motka <70910839+tushar2481@users.noreply.github.com>
2022-10-16 23:08:25 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ function nmap() {
     document.getElementById('ip1').style.display = 'block';
     document.getElementById('ip2').style.display = 'block';
     document.getElementById('ip3').style.display = 'block';
+    document.getElementById('sub-btn').style.display = 'block';
     document.getElementById('tool_name').value = "nmap";
     document.getElementById('ip1').placeholder = "Enter IP Address";
     document.getElementById('ip2').placeholder = "Number of threads (1-5)";
@@ -14,6 +15,7 @@ function assetfinder() {
     document.getElementById('ip1').style.display = 'block';
     document.getElementById('ip2').style.display = 'none';
     document.getElementById('ip3').style.display = 'none';
+    document.getElementById('sub-btn').style.display = 'block';
     document.getElementById('tool_name').value = "assetfinder";
     document.getElementById('ip1').placeholder = "Enter Domain Name";
 }
@@ -23,6 +25,7 @@ function dnsenum() {
     document.getElementById('ip1').style.display = 'block';
     document.getElementById('ip2').style.display = 'block';
     document.getElementById('ip3').style.display = 'block';
+    document.getElementById('sub-btn').style.display = 'block';
     document.getElementById('tool_name').value = "dnsenum";
     document.getElementById('ip1').placeholder = "Enter Domain Name";
     document.getElementById('ip2').placeholder = "Enable Zone Transfer? (Y/n)";
