5cd4c83de1ba76981eb250d23f612f8227764272
tushar motka <70910839+tushar2481@users.noreply.github.com>
2022-10-16 23:03:49 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ function nmap() {
     document.getElementById('ip1').style.display = 'block';
     document.getElementById('ip2').style.display = 'block';
     document.getElementById('ip3').style.display = 'block';
+    document.getElementById('tool_name').value = "nmap";
     document.getElementById('ip1').placeholder = "Enter IP Address";
     document.getElementById('ip2').placeholder = "Number of threads (1-5)";
     document.getElementById('ip3').placeholder = "Verbose Mode (yes/no)";
@@ -13,6 +14,7 @@ function assetfinder() {
     document.getElementById('ip1').style.display = 'block';
     document.getElementById('ip2').style.display = 'none';
     document.getElementById('ip3').style.display = 'none';
+    document.getElementById('tool_name').value = "assetfinder";
     document.getElementById('ip1').placeholder = "Enter Domain Name";
 }
 
@@ -21,6 +23,7 @@ function dnsenum() {
     document.getElementById('ip1').style.display = 'block';
     document.getElementById('ip2').style.display = 'block';
     document.getElementById('ip3').style.display = 'block';
+    document.getElementById('tool_name').value = "dnsenum";
     document.getElementById('ip1').placeholder = "Enter Domain Name";
     document.getElementById('ip2').placeholder = "Enable Zone Transfer? (Y/n)";
     document.getElementById('ip3').placeholder = "Enable Verbose Mode? (Y/n)";
