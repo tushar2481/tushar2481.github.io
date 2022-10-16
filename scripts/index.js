276547aeb70c25417214066df82de03a6a627805
tushar motka <70910839+tushar2481@users.noreply.github.com>
2022-10-17 00:00:14 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -10,21 +10,21 @@ function nmap() {
 }
 
 function assetfinder() {
+    document.getElementById('ip1').placeholder = "Enter Domain Name";
+    document.getElementById('tool_name').setAttribute('value', 'assetfinder');
     document.getElementById('ip1').style.display = 'block';
     document.getElementById('ip2').style.display = 'none';
     document.getElementById('ip3').style.display = 'none';
     document.getElementById('sub-btn').style.display = 'block';
-    document.getElementById('tool_name').setAttribute('value', 'assetfinder');
-    document.getElementById('ip1').placeholder = "Enter Domain Name";
 }
 
 function dnsenum() {
-    document.getElementById('ip1').style.display = 'block';
-    document.getElementById('ip2').style.display = 'block';
-    document.getElementById('ip3').style.display = 'block';
-    document.getElementById('sub-btn').style.display = 'block';
     document.getElementById('tool_name').setAttribute('value', 'dnsenum');
     document.getElementById('ip1').placeholder = "Enter Domain Name";
     document.getElementById('ip2').placeholder = "Enable Zone Transfer? (Y/n)";
     document.getElementById('ip3').placeholder = "Enable Verbose Mode? (Y/n)";
+    document.getElementById('ip1').style.display = 'block';
+    document.getElementById('ip2').style.display = 'block';
+    document.getElementById('ip3').style.display = 'block';
+    document.getElementById('sub-btn').style.display = 'block';
 }
