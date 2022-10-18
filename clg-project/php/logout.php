ca3437983edfb14057d304663613c39f520fa2ef
tushar motka <70910839+tushar2481@users.noreply.github.com>
2022-10-19 01:02:10 +0530

    Delete clg-project directory

--- clg-project/php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-$cookie_name = "cookie";
-$cookie_value = null;
-    setcookie($cookie_name, $cookie_value, time() - (86400 * 30), "/");
-    header("Location: /index.html");
-    session_abort();
-?>
